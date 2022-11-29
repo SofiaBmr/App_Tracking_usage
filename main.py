885495621513c9d8529f8b42e1b4d4f4c60020fb
SofiaBmr <sofia.boumahrat@epfedu.fr>
2022-11-29 15:36:53 +0100

    add the google tag script

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ app=Flask(__name__)
 
 
 def hello_world():
-	prefix_google="""
-	<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-250382412-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-250382412-1');
-</script>
+  prefix_google="""
+	<!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-250382412-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-250382412-1');
+  </script>
 """
-    return prefix_google + "Hello, world!"
+  return prefix_google + "Hello, world!"
