ae4fb8607630067b37015fe1ed00e93452374e78
SofiaBmr <sofia.boumahrat@epfedu.fr>
2022-12-20 15:55:39 +0100

    tp2 cookies adding

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 - Create a sample python app on Deta using Flask 
 - Track usage with Google Analytics
+- Add a page that print log in the browser console
+- Manipulate cookies
+
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask,render_template,make_response,request
 import logging
+import requests
 app=Flask(__name__)
 @app.route('/')
 
@@ -15,18 +16,58 @@ def hello_world():
     gtag('js', new Date());
 
     gtag('config', 'UA-250382412-1');
+    req =requests.get("https://www.google.com/");
+    return req.cookies.get_dict()
   </script>
-  <button type="button">Click Me!</button></html>
+  <input type="button" onclick="window.location.href = './loggs';" value="Click here to see the log page"/>
+  </html>
 """
   return prefix_google
 
 
-# tp2: log part
+# tp2: log part and text box
 @app.route('/loggs')
 def loggs():
     app.logger.warning('testing warning log')
     script="""
     <script>console.log("loggy loggy where am I ?")</script>
-    """
-    return "Check your console "+ script
 
+    <form action="/setcookies" method="POST">
+      <label for="uname">Username :</label>
+      <input type="text" id="uname" name="uname" placeholder="Sofia"required><br><br>
+      <label for="pwd">Password :</label>
+      <input type="text" id="pwd" name="pwd" placeholder="viveladata"required><br><br>
+      <input type="submit" value="Submit">
+    </form>
+
+    """
+    return "Check your console to see the logs"+ "<br><br>" +script
+
+#set the cookie of the username
+@app.route('/setcookies', methods = ['POST', 'GET'])
+def setcookie():
+  if request.method == 'POST':
+    username = request.form['uname']
+    password= request.form['pwd']
+  script="""
+  <input type="button" onclick="window.location.href = './getcook';" value="Click here to see the cookie page"/>
+  <input type="button" onclick="window.location.href = './getcook2';" value="Click here to see the analytic page"/>
+  """
+  return "Welcome " + username + " !" + script
+
+#request https://www.google.com/
+@app.route('/cookie_test')
+def cookie_test():
+  req =requests.get("https://www.google.com/")
+  return req.status_code
+
+
+@app.route('/getcook')
+def get_cookies():
+  req =requests.get("https://analytics.google.com/analytics/web/#/report-home/a250382412w344211401p280829293")
+  return req.cookies.get_dict()
+
+@app.route('/getcook2')
+def get_cookies2():
+  req =requests.get("https://analytics.google.com/analytics/web/#/report-home/a250382412w344211401p280829293")
+  return req.text
